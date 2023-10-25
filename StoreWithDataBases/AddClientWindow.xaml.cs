cffbd364f1e4c5c99e4fb9dd7c12c15dca35dfb1
AndrewKooklin <bolt999@yandex.ru>
2023-10-25 14:09:55 +0300

    Add AddWindow

---
+++ StoreWithDataBases/AddClientWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace StoreWithDataBases
+{
+    /// <summary>
+    /// Interaction logic for AddClientWindow.xaml
+    /// </summary>
+    public partial class AddClientWindow : Window
+    {
+        public AddClientWindow()
+        {
+            InitializeComponent();
+        }
+
+        public AddClientWindow(DataRow dataRow) : this()
+        {
+            btnCancel.Click += delegate { this.DialogResult = false; };
+            btnClearForm.Click += delegate
+            {
+                tbLastName.Text = "";
+                tbFirstName.Text = "";
+                tbFathersName.Text = "";
+                tbPhoneNumber.Text = "";
+                tbEMail.Text = "";
+            };
+
+            btnAddClient.Click += delegate
+            {
+                dataRow["LastName"] = tbLastName.Text;
+                dataRow["FirstName"] = tbFirstName.Text;
+                dataRow["FathersName"] = tbFathersName.Text;
+                dataRow["PhoneNumber"] = tbPhoneNumber.Text;
+                dataRow["EMail"] = tbEMail.Text;
+                this.DialogResult = !false;
+            };
+        }
+    }
+}
--- StoreWithDataBases/ClientsWindow.xaml.cs
+++ StoreWithDataBases/ClientsWindow.xaml.cs
@@ -8,12 +8,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace StoreWithDataBases
 {
@@ -36,12 +30,53 @@ namespace StoreWithDataBases
         {
             dT = new DataTable();
             sqlDA = new SqlDataAdapter();
+
+            //SelectCommand
             string query = $"SELECT Id, LastName, FirstName, FathersName," +
                             $"PhoneNumber, EMail FROM Clients";
             connectToSQLDB = new ConnectToSQLDB();
             sqlDA.SelectCommand = new SqlCommand(query, connectToSQLDB.GetSqlConnection());
+            //EndSelectCommand
 
+            //InsertCommand
+            query = @"INSERT INTO Clients (LastName, FirstName, FathersName, PhoneNumber, EMail) 
+                                 VALUES (@LastName, @FirstName,  @FathersName,  @PhoneNumber, @EMail);
+                                SET @id = @@IDENTITY;";
 
+            sqlDA.InsertCommand = new SqlCommand(query, connectToSQLDB.GetSqlConnection());
+
+            sqlDA.InsertCommand.Parameters.Add("@id", SqlDbType.Int, 4, "id").Direction = ParameterDirection.Output;
+            sqlDA.InsertCommand.Parameters.Add("@LastName", SqlDbType.VarChar, 50, "LastName");
+            sqlDA.InsertCommand.Parameters.Add("@FirstName", SqlDbType.VarChar, 50, "FirstName");
+            sqlDA.InsertCommand.Parameters.Add("@FathersName", SqlDbType.VarChar, 50, "FathersName");
+            sqlDA.InsertCommand.Parameters.Add("@PhoneNumber", SqlDbType.VarChar, 50, "PhoneNumber");
+            sqlDA.InsertCommand.Parameters.Add("@EMail", SqlDbType.VarChar, 50, "EMail");
+            //EndInsertCommand
+
+            //UpdateCommand
+            query = @"UPDATE Clients SET 
+                           LastName = @wLastName,
+                           FirstName = @FirstName, 
+                           FathersName = @FathersName,
+                           PhoneNumber = @PhoneNumber,
+                           EMail = @EMail 
+                    WHERE Id = @Id";
+
+            sqlDA.UpdateCommand = new SqlCommand(query, connectToSQLDB.GetSqlConnection());
+            sqlDA.UpdateCommand.Parameters.Add("@Id", SqlDbType.Int, 4, "Id").SourceVersion = DataRowVersion.Original;
+            sqlDA.UpdateCommand.Parameters.Add("@LastName", SqlDbType.VarChar, 50, "LastName");
+            sqlDA.UpdateCommand.Parameters.Add("@FirstName", SqlDbType.VarChar, 50, "FirstName");
+            sqlDA.UpdateCommand.Parameters.Add("@FathersName", SqlDbType.VarChar, 50, "FathersName");
+            sqlDA.UpdateCommand.Parameters.Add("@PhoneNumber", SqlDbType.VarChar, 50, "PhoneNumber");
+            sqlDA.UpdateCommand.Parameters.Add("@EMail", SqlDbType.VarChar, 50, "EMail");
+            //EndUpdateCommand
+
+            //DeleteCommand
+            query = "DELETE FROM Clients WHERE Id = @Id";
+
+            sqlDA.DeleteCommand = new SqlCommand(query, connectToSQLDB.GetSqlConnection());
+            sqlDA.DeleteCommand.Parameters.Add("@Id", SqlDbType.Int, 4, "Id");
+            //EndDeleteCommand
 
             sqlDA.Fill(dT);
             dgClients.DataContext = dT.DefaultView;
@@ -49,12 +84,55 @@ namespace StoreWithDataBases
 
         private void CurrentCell_Changed(object sender, EventArgs e)
         {
-
+            if (rowView == null) return;
+            rowView.EndEdit();
+            sqlDA.Update(dT);
         }
 
         private void CellEdit_Ending(object sender, DataGridCellEditEndingEventArgs e)
         {
+            rowView = (DataRowView)dgClients.SelectedItem;
+            rowView.BeginEdit();
+        }
 
+        private void MIAddClient_Click(object sender, RoutedEventArgs e)
+        {
+            DataRow dR = dT.NewRow();
+            AddClientWindow addClientWindow = new AddClientWindow(dR);
+            addClientWindow.ShowDialog();
+
+
+            if (addClientWindow.DialogResult.Value)
+            {
+                dT.Rows.Add(dR);
+                sqlDA.Update(dT);
+            }
+        }
+
+        private void MIDeleteClient_Click(object sender, RoutedEventArgs e)
+        {
+            if (dgClients.SelectedIndex == -1 || rowView == null)
+            {
+                MessageBox.Show("Select client", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            else
+            {
+                rowView = (DataRowView)dgClients.SelectedItem;
+                rowView.Row.Delete();
+                sqlDA.Update(dT);
+            }
+        }
+
+        private void ClientsWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            dgClients.SelectedItem = null;
+        }
+
+        private void ClientsWindow_Unloaded(object sender, RoutedEventArgs e)
+        {
+            connectToSQLDB.CloseSQLConnection();
+            dgClients.DataContext = null;
         }
     }
 }
